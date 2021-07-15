b3944abcaf5a0413eb876c834640aa9129518cca
Kid <theo.morra@m6securities.com>
2021-07-16 00:38:46 +1200

    significant performance improvements

--- client/worker.js
+++ client/worker.js
@@ -1,3 +1,7 @@
+const SharedComms = new BroadcastChannel('comms')
+let base = "https://localhost:8001", myUri = "https://localhost:8001", dataQueue = {}
+
+
 function checkScheme(url) {
     if (!/^(?:f|ht)tps?\:\/\//.test(url)) {
         url = "http://" + url
@@ -5,58 +9,57 @@ function checkScheme(url) {
     return url
 }
 
-function doAsyncPromise(url) {
-    return new Promise(function (fulfill, reject) {
-        var ws = new WebSocket('ws://localhost:8000')
-        ws.onopen = function () {
-            console.log('sent')
-            ws.send(JSON.stringify({'type': 'loadLibraryAsRaw', 'uri': url}))
-        }
+async function waitForData(url){
+    while(!dataQueue[url]){
+        await new Promise(resolve => setTimeout(resolve, 100))
+    }
 
-        ws.onerror = function (error) {
-            console.log('WebSocket error: ' + error)
-        }
-        ws.onmessage = function (message) {
-            let content
-            console.log('recv')
-            let context = JSON.parse(message.data)
-            if (context.type === "rawContent") {
-                content = true
-                if (context.contentType.includes('image') || context.contentType.includes('video') || context.contentType.includes('audio') || context.contentType.includes('font')){
-                    let binaryImg = atob(context.content)
-                    let length = binaryImg.length
-                    let ab = new ArrayBuffer(length)
-                    let ua = new Uint8Array(ab)
-                    for (let i = 0; i < length; i++) {
-                        ua[i] = binaryImg.charCodeAt(i)
-                    }
-                    content = new Blob([ab], {
-                        type: context.contentType
-                    })
-                } else {
-                    content = atob(context.content)
-                }
-                ws.close()
-                fulfill(new Response(content, { headers: { 'Content-Type': context.contentType }}))
-            }
-        }
-    })
+    return dataQueue[url]
 }
 
-var base = "https://localhost:8001"
+function doAsyncPromise(url) {
+    SharedComms.postMessage(url)
+
+    SharedComms.onmessage = function (message) {
+        let context = JSON.parse(message.data)
+        dataQueue[context.uri] = context
+    }
+    
+    return new Promise(async function (fulfill, reject) {
+        context = await waitForData(url)
+        let content = true
+        console.log(`recv: ${context.uri}`)
+        if (context.contentType.includes('image') || context.contentType.includes('video') || context.contentType.includes('audio') || context.contentType.includes('font')){
+            let binaryImg = atob(context.content)
+            let length = binaryImg.length
+            let ab = new ArrayBuffer(length)
+            let ua = new Uint8Array(ab)
+            for (let i = 0; i < length; i++) {
+                ua[i] = binaryImg.charCodeAt(i)
+            }
+            content = new Blob([ab], {
+                type: context.contentType
+            })
+        } else {
+            content = atob(context.content)
+        }
+        fulfill(new Response(content, {headers: {'Content-Type': context.contentType}}))
+    })
+    
+}
 
 self.addEventListener('fetch', async function(event) {
     if (event.isReload) {
         base = "https://localhost:8001"
     }
     base_url = new URL(base)
-    let url = event.request.url.replace('https://localhost:8001', '')[0] === '/' ? checkScheme(base_url.origin + event.request.url.replace('https://localhost:8001', '')) : event.request.url
+    let url = event.request.url.replace(myUri, '')[0] === '/' ? checkScheme(base_url.origin + event.request.url.replace(myUri, '')) : event.request.url
 
     if (url.includes('localhost') || url.includes('s3.theom.nz') || url.includes('code.jquery.com')) {
         console.log("NOT serving request: ", url)
         event.respondWith(fetch(url))
     } else {
-        console.log("Serving request:", event.request.url.replace('https://localhost:8001', ''))
+        console.log("Serving request:", event.request.url.replace(myUri, ''))
         event.respondWith(
             doAsyncPromise(url).then((res) => {
                 return res
