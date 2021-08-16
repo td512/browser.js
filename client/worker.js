430430bc69c2a84c359a35f9074205cc039f287e
Kid <github@theom.nz>
2021-08-16 12:16:07 +1200

    TODO: Finish worker.js comments

--- client/worker.js
+++ client/worker.js
@@ -1,8 +1,12 @@
+// open the comms BroadcastChannel
 const SharedComms = new BroadcastChannel('comms')
+// on initial load set the base URLs, and instantiate objects that need to be created
 let base = "http://localhost:8001", myUri = "http://localhost:8001", dataQueue = {}
 
-
+// this is the same function as addhttp in the client side script
 function checkScheme(url) {
+    // checkScheme ingests a URL and uses a regex test to check if the url has http:// or https:// in the URL sent, and
+    // modifies the URL on the fly. This is done to avoid breaking the server
     if (!/^(?:f|ht)tps?\:\/\//.test(url)) {
         url = "http://" + url
     }
@@ -10,10 +14,16 @@ function checkScheme(url) {
 }
 
 async function waitForData(url){
+    // waitForData exploits JavaScript promises by checking for the dataqueue[url]
+    // and if it doesn't exist awaits a new promise that will eventually resolve
+    // when the data finally arrives. This was added because requests would resolve in the wrong order
+    // and return junk data
     while(!dataQueue[url]){
         await new Promise(resolve => setTimeout(resolve, 100))
     }
 
+    // because of the way promise awaits work, by the time we get down here, the data already exists, so we don't need
+    // to check for it, since it's guaranteed to exist
     return dataQueue[url]
 }
 
@@ -67,7 +77,9 @@ self.addEventListener('fetch', async function(event) {
     }
 })
 
+// on data posted to us by the client side...
 self.addEventListener("message", function(event) {
+    // DEBUG: update the base URL, and log it
     base = event.data
     console.log('New base URL: ' + base)
 })
